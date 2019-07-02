f1deee4e83862fdac16c66bc501da2a2b2ddc3ed
Chen Guo <almightygc@gmail.com>
2019-07-02 16:53:08 -0700

    add word count example for azure Spark HDI

--- chen-spark-jobs/src/main/scala/com/chen/guo/SparkWordCount.scala
+++ chen-spark-jobs/src/main/scala/com/chen/guo/SparkWordCount.scala
@@ -1,18 +1,36 @@
 package com.chen.guo
 
+import org.apache.hadoop.fs.Path
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object SparkWordCount extends App {
+  for (arg <- args) {
+    println(s"arg: $arg")
+  }
+
   val ss: SparkSession =
     SparkSession.builder()
-      .master("local")
-      .appName("test")
+      .appName("word count")
       //.enableHiveSupport()
       .getOrCreate()
 
-  val textFile = ss.sparkContext.textFile("hdfs://...")
-  val counts = textFile.flatMap(line => line.split(" "))
+  val sc = ss.sparkContext
+
+  var inputs: RDD[String] = sc.emptyRDD[String]
+  for (arg <- args.dropRight(1)) {
+    println(s"Creating RDD for: $arg")
+    inputs = inputs.union(sc.textFile(arg))
+  }
+
+  val counts: RDD[(String, Int)] = inputs.flatMap(line => line.split(" "))
     .map(word => (word, 1))
     .reduceByKey(_ + _)
-  counts.saveAsTextFile("hdfs://...")
+
+  val dest = args.last
+  val outputDir = new Path(dest, System.currentTimeMillis.toString)
+  println(s"Saving output to: $outputDir")
+
+  counts.coalesce(1).saveAsTextFile(outputDir.toString)
+  println("Done")
 }
