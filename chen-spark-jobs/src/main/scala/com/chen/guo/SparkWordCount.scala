0579e205c0e3401b8e8d1573da72aba42914913f
Chen Guo <almightygc@gmail.com>
2019-07-30 10:44:08 -0700

    tmp

--- chen-spark-jobs/src/main/scala/com/chen/guo/SparkWordCount.scala
+++ chen-spark-jobs/src/main/scala/com/chen/guo/SparkWordCount.scala
@@ -1,5 +1,10 @@
 package com.chen.guo
 
+import java.io.StringReader
+
+import com.chen.guo.security.KeyVaultADALAuthenticator
+import com.microsoft.azure.keyvault.KeyVaultClient
+import com.microsoft.azure.storage.CloudStorageAccount
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
@@ -27,10 +32,28 @@ object SparkWordCount extends App {
     .map(word => (word, 1))
     .reduceByKey(_ + _)
 
-  val dest = args.last
+  val dest = args(args.length - 2)
   val outputDir = new Path(dest, System.currentTimeMillis.toString)
   println(s"Saving output to: $outputDir")
 
   counts.coalesce(1).saveAsTextFile(outputDir.toString)
   println("Done")
+
+  val storageAccountConnectionString = args(args.length - 1)
+  System.out.println(String.format("Storage Account Connection String: %s", storageAccountConnectionString))
+  val storageAccount = CloudStorageAccount.parse(storageAccountConnectionString)
+  val cloudBlobClient = storageAccount.createCloudBlobClient
+  val container = cloudBlobClient.getContainerReference("demo-jars")
+  val blockRef = container.getBlockBlobReference("properties/azure_credentials.properties")
+  val s = blockRef.downloadText
+  System.out.println("Blob Content: " + s)
+  val credentials = new CredentialsFileProvider(new StringReader(s))
+  val sp = "key-vault-manager"
+  //this sp must be granted access in the KV's Access Policies
+  val vaultURL = "https://chen-vault.vault.azure.net/"
+
+  val kvClient = new KeyVaultClient(KeyVaultADALAuthenticator.createCredentials(credentials, sp))
+  val secret = kvClient.getSecret(vaultURL, "sas-token")
+  System.out.println("Fetched SAS token: " + secret.value)
+
 }
