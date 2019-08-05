726966689337ba87c9fb808cd14ff83e47a8743a
Chen Guo <almightygc@gmail.com>
2019-08-05 14:58:20 -0700

    a

--- chen-spark-jobs/src/main/scala/com/chen/guo/SparkWordCount2.scala
+++ chen-spark-jobs/src/main/scala/com/chen/guo/SparkWordCount2.scala
@@ -1,12 +1,12 @@
 package com.chen.guo
 
-import java.text.SimpleDateFormat
-
 import org.apache.hadoop.fs.Path
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import org.slf4j.{Logger, LoggerFactory}
 
 object SparkWordCount2 extends App {
+  val logger: Logger = LoggerFactory.getLogger(getClass.getName)
   /**
     * 1st arg: full eg path
     * 2nd arg: partial customer raw data path
@@ -19,7 +19,7 @@ object SparkWordCount2 extends App {
     * To ignore this error, set spark.driver.allowMultipleContexts = true.
     */
   for (arg <- args) {
-    println(s"arg: $arg")
+    logger.info(s"arg: $arg")
   }
   val storageAccountConnectionString = args(args.length - 2)
 
@@ -33,7 +33,7 @@ object SparkWordCount2 extends App {
     val clientName = cn.toLowerCase()
     val clientId = cosmos(clientName)
     val clientSecret = CredentialsFileProvider.getSecretFromSA(storageAccountConnectionString, clientName + "-secret")
-    println(s"Got id $clientId secret $clientSecret for $clientName")
+    logger.info(s"Got id $clientId secret $clientSecret for $clientName")
 
     val ss: SparkSession =
       SparkSession.builder()
@@ -49,7 +49,7 @@ object SparkWordCount2 extends App {
 
     if (clientName != clientNamesArray(0)) {
       //session's conf v.s. sc's conf?
-      println(s"Changing settings")
+      logger.info(s"Changing settings")
       ss.conf.set("spark.hadoop.fs.azure.account.auth.type", "OAuth")
       ss.conf.set("spark.hadoop.fs.azure.account.oauth.provider.type", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
       ss.conf.set("spark.hadoop.fs.azure.account.oauth2.client.id", clientId)
@@ -57,37 +57,32 @@ object SparkWordCount2 extends App {
       ss.conf.set("spark.hadoop.fs.azure.account.oauth2.client.endpoint", "https://login.microsoftonline.com/2445f142-5ffc-43aa-b7d2-fb14d30c8bd3/oauth2/token")
     }
 
-    println(s"Printing configurations for $clientName")
-    ss.conf.getAll.foreach(x => if (x._1.startsWith("spark.hadoop")) println(s"${x._1} -> ${x._2}"))
-    println(s"spark.master -> ${ss.conf.get("spark.master")}")
+    logger.info(s"Printing configurations for $clientName")
+    ss.conf.getAll.foreach(x => if (x._1.startsWith("spark.hadoop")) logger.info(s"${x._1} -> ${x._2}"))
+    logger.info(s"spark.master -> ${ss.conf.get("spark.master")}")
 
     val sc = ss.sparkContext
     var inputs: RDD[String] = sc.emptyRDD[String]
     val egDataFullPath = args(0)
-    println(s"In application ${sc.applicationId}: Get EG data path $egDataFullPath")
+    logger.info(s"In application ${sc.applicationId}: Get EG data path $egDataFullPath")
     inputs.union(sc.textFile(egDataFullPath))
 
     for (inputPathPart <- args.tail.dropRight(3)) {
       val path = s"abfss://$clientName@$inputPathPart"
-      println(s"Get Raw data full path: $path")
+      logger.info(s"Get Raw data full path: $path")
       inputs = inputs.union(sc.textFile(path))
     }
 
     val dest = s"abfss://$clientName@${args(args.length - 3)}"
     val outputDir = new Path(dest, System.currentTimeMillis.toString)
-    println(s"${getTime()} Will save output to: $outputDir")
+    logger.info(s"Will save output to: $outputDir")
 
     val counts: RDD[(String, Int)] = inputs.flatMap(line => line.split(" "))
       .map(word => (word, 1))
       .reduceByKey(_ + _)
 
-    println(s"${getTime()} Got counts")
+    logger.info(s"Got counts")
     counts.coalesce(1).saveAsTextFile(outputDir.toString)
-    println(s"${getTime()} Done for $clientName")
+    logger.info(s"Done for $clientName")
   })
-
-  private def getTime(): String = {
-    val dateTime = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSZ")
-    dateTime.format(System.currentTimeMillis())
-  }
 }
