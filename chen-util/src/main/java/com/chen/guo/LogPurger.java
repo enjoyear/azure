a02879223103b05b9e134a8c57d70b6179059e12
Chen Guo <almightygc@gmail.com>
2019-08-21 16:18:18 -0700

    a

--- chen-util/src/main/java/com/chen/guo/LogPurger.java
+++ chen-util/src/main/java/com/chen/guo/LogPurger.java
@@ -4,42 +4,78 @@ import com.chen.guo.storage.BlobBasics;
 import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.blob.*;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
+import java.util.Date;
 
+/**
+ * See examples here
+ * https://github.com/Azure-Samples/storage-blob-java-getting-started/blob/master/src/BlobBasics.java
+ * <p>
+ * Create back-up container
+ * azcopy cp "https://abs0execution.blob.core.windows.net/sparkjobs/*" "https://abs0execution.blob.core.windows.net/sparkjobs-copy/" --recursive=true
+ * azcopy cp "https://abs0execution.blob.core.windows.net/sparkjobs/*" "https://abs0execution.blob.core.windows.net/sparkjobs-copy/" --recursive=true
+ */
 @Slf4j
 public class LogPurger {
-  public static void main(String[] args) throws InvalidKeyException, StorageException, URISyntaxException, IOException {
-    LogPurger logPurger = new LogPurger("as");
-    logPurger.printBlobs("sparkjobs");
+  public static void main(String[] args) throws InvalidKeyException, StorageException, URISyntaxException {
+    DateTime jodaDate = new DateTime(2019, 8, 21, 17, 20);
+    Date date = jodaDate.toDate();
+    String saConnectionString = "";
+    LogPurger logPurger = new LogPurger(saConnectionString, "sparkjobs", date);
+    logPurger.printBlobs();
   }
 
   private final String _storageAccountConnectionString;
+  private final Date _purgeDate;
+  private final CloudBlobContainer _container;
 
-  public LogPurger(String saConnectionString) {
+  /**
+   * @param saConnectionString
+   * @param purgeDate          logs older than purgeDate will be purged
+   */
+  public LogPurger(String saConnectionString, String containerName, Date purgeDate) throws InvalidKeyException, StorageException, URISyntaxException {
     _storageAccountConnectionString = saConnectionString;
-
+    _purgeDate = purgeDate;
+    _container = new BlobBasics(_storageAccountConnectionString).getBlobContainer(containerName);
   }
 
-  public void printBlobs(String containerName) throws InvalidKeyException, StorageException, URISyntaxException {
-    CloudBlobContainer container = getContainer(containerName);
-    //Get the flat list
-    Iterable<ListBlobItem> rootDirs = container.listBlobs("", true);
-    //Iterable<ListBlobItem> rootDirs = container.listBlobs();
+  public void printBlobs() throws URISyntaxException, StorageException {
+    long start = System.currentTimeMillis();
+    int count = flatScan();
+    //int count = dfsScan(container);
+    log.info(String.format("Take %.1f seconds to get a total of %s blobs", (System.currentTimeMillis() - start) / 1000.0, count));
+  }
 
-    int[] count = new int[]{0};
-    for (ListBlobItem rootDir : rootDirs) {
-      dfsTraverseBlobItem(container, rootDir, count);
-      //CloudBlockBlob blockRef = container.getBlockBlobReference(rootPath);
-      //System.out.println(blockRef);
+  /**
+   * Scan the container in FlatBlobListing mode
+   *
+   * @return the number of blobs
+   */
+  private int flatScan() throws URISyntaxException, StorageException {
+    int count = 0;
+    Iterable<ListBlobItem> blobs = _container.listBlobs("", true);
+    for (ListBlobItem blob : blobs) {
+      ++count;
+      blobPrint(blob);
     }
-    log.info(String.format("Get a total of %s blobs(folders and files)", count[0]));
+    return count;
   }
 
-  public CloudBlobContainer getContainer(String containerName) throws InvalidKeyException, StorageException, URISyntaxException {
-    return new BlobBasics(_storageAccountConnectionString).getBlobContainer(containerName);
+  /**
+   * Scan the container in FlatBlobListing mode
+   *
+   * @return the number of dirs and blobs
+   */
+  private int dfsScan() throws URISyntaxException, StorageException {
+    int[] count = new int[]{0};
+    Iterable<ListBlobItem> rootDirs = _container.listBlobs();
+    for (ListBlobItem rootDir : rootDirs) {
+      dfsTraverseBlobItem(rootDir, count);
+    }
+    return count[0];
   }
 
   /**
@@ -50,26 +86,40 @@ public class LogPurger {
    *
    * @param item the entry item for the traversal
    */
-  private void dfsTraverseBlobItem(CloudBlobContainer container, ListBlobItem item, int[] count) {
+  private void dfsTraverseBlobItem(ListBlobItem item, int[] count) throws URISyntaxException, StorageException {
     ++count[0];
 
     if (item instanceof CloudBlobDirectory) {
       CloudBlobDirectory dir = (CloudBlobDirectory) item;
 //      log.info(String.format("%s: %s", dir.getPrefix(), dir.getUri().getPath()));
-      Iterable<ListBlobItem> blobs = container.listBlobs(dir.getPrefix());
+      Iterable<ListBlobItem> blobs = _container.listBlobs(dir.getPrefix());
 //      List<ListBlobItem> b = StreamSupport.stream(blobs.spliterator(), false).collect(Collectors.toList());
 
       for (ListBlobItem blobItem : blobs) {
-        dfsTraverseBlobItem(container, blobItem, count);
+        dfsTraverseBlobItem(blobItem, count);
       }
     } else {
       if (item instanceof CloudBlockBlob) {
-        CloudBlockBlob blob = (CloudBlockBlob) item;
-        BlobProperties properties = blob.getProperties();
-        log.info(String.format("%s: %s", item.getUri().getPath(), properties.getLastModified()));
+        blobPrint(item);
       } else {
         throw new RuntimeException("What is this? " + item.getClass().getSimpleName() + ": " + item.getUri().getPath());
       }
+    }
+  }
+
+  private void blobPrint(ListBlobItem item) throws URISyntaxException, StorageException {
+    CloudBlockBlob blob = (CloudBlockBlob) item;
+    BlobProperties properties = blob.getProperties();
+    Date lastModified = properties.getLastModified();
+    //String path = item.getUri().getPath();
+    String path = blob.getName();
+
+    if (lastModified.before(_purgeDate)) {
+      log.info(String.format("[Deleting]%s: %s", path, lastModified));
+      CloudBlockBlob blockRef = _container.getBlockBlobReference(path);
+      blockRef.delete();
+    } else {
+      log.info(String.format("[Keep]%s: %s", path, lastModified));
     }
   }
 }
