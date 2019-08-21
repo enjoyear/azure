5de2c34c65e4424c5c4ad9ecc4f3f4d2d66d2914
Chen Guo <almightygc@gmail.com>
2019-08-21 10:59:32 -0700

    a

--- chen-util/src/main/java/com/chen/guo/LogPurger.java
+++ chen-util/src/main/java/com/chen/guo/LogPurger.java
@@ -2,35 +2,74 @@ package com.chen.guo;
 
 import com.chen.guo.storage.BlobBasics;
 import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlobDirectory;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
-import com.microsoft.azure.storage.blob.ListBlobItem;
+import com.microsoft.azure.storage.blob.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 
+@Slf4j
 public class LogPurger {
   public static void main(String[] args) throws InvalidKeyException, StorageException, URISyntaxException, IOException {
-    String storageAccountConnectionString = "";
-    CloudBlobContainer container = new BlobBasics(storageAccountConnectionString).getBlobContainer("sparkjobs");
-    Iterable<ListBlobItem> rootDirs = container.listBlobs();
+    LogPurger logPurger = new LogPurger("as");
+    logPurger.printBlobs("sparkjobs");
+  }
+
+  private final String _storageAccountConnectionString;
+
+  public LogPurger(String saConnectionString) {
+    _storageAccountConnectionString = saConnectionString;
+
+  }
+
+  public void printBlobs(String containerName) throws InvalidKeyException, StorageException, URISyntaxException {
+    CloudBlobContainer container = getContainer(containerName);
+    //Get the flat list
+    Iterable<ListBlobItem> rootDirs = container.listBlobs("", true);
+    //Iterable<ListBlobItem> rootDirs = container.listBlobs();
+
+    int[] count = new int[]{0};
     for (ListBlobItem rootDir : rootDirs) {
-      String rootPath = rootDir.getUri().getPath();
-      //URI: https://abs0execution.blob.core.windows.net/sparkjobs/3d3d8d23-d45d-4c51-b55f-a1778b7ed920/
-      //Path: /sparkjobs/3d3d8d23-d45d-4c51-b55f-a1778b7ed920/
-      //Prefix: 3d3d8d23-d45d-4c51-b55f-a1778b7ed920/
-      System.out.println(rootDir.getClass().getSimpleName() + ": " + rootPath);
-      if (rootDir instanceof CloudBlobDirectory) {
-        CloudBlobDirectory dir = (CloudBlobDirectory) rootDir;
-        Iterable<ListBlobItem> blobs = container.listBlobs(dir.getPrefix());
-        for (ListBlobItem blob : blobs) {
-          System.out.println(blob.getClass().getSimpleName() + ": " + blob.getUri().getPath());
-        }
-      }
-      CloudBlockBlob blockRef = container.getBlockBlobReference(rootPath);
+      dfsTraverseBlobItem(container, rootDir, count);
+      //CloudBlockBlob blockRef = container.getBlockBlobReference(rootPath);
       //System.out.println(blockRef);
+    }
+    log.info(String.format("Get a total of %s blobs(folders and files)", count[0]));
+  }
+
+  public CloudBlobContainer getContainer(String containerName) throws InvalidKeyException, StorageException, URISyntaxException {
+    return new BlobBasics(_storageAccountConnectionString).getBlobContainer(containerName);
+  }
+
+  /**
+   * Field value examples:
+   * URI: https://abs0execution.blob.core.windows.net/sparkjobs/3d3d8d23-d45d-4c51-b55f-a1778b7ed920/
+   * Path: /sparkjobs/3d3d8d23-d45d-4c51-b55f-a1778b7ed920/
+   * Prefix: 3d3d8d23-d45d-4c51-b55f-a1778b7ed920/
+   *
+   * @param item the entry item for the traversal
+   */
+  private void dfsTraverseBlobItem(CloudBlobContainer container, ListBlobItem item, int[] count) {
+    ++count[0];
+
+    if (item instanceof CloudBlobDirectory) {
+      CloudBlobDirectory dir = (CloudBlobDirectory) item;
+//      log.info(String.format("%s: %s", dir.getPrefix(), dir.getUri().getPath()));
+      Iterable<ListBlobItem> blobs = container.listBlobs(dir.getPrefix());
+//      List<ListBlobItem> b = StreamSupport.stream(blobs.spliterator(), false).collect(Collectors.toList());
+
+      for (ListBlobItem blobItem : blobs) {
+        dfsTraverseBlobItem(container, blobItem, count);
+      }
+    } else {
+      if (item instanceof CloudBlockBlob) {
+        CloudBlockBlob blob = (CloudBlockBlob) item;
+        BlobProperties properties = blob.getProperties();
+        log.info(String.format("%s: %s", item.getUri().getPath(), properties.getLastModified()));
+      } else {
+        throw new RuntimeException("What is this? " + item.getClass().getSimpleName() + ": " + item.getUri().getPath());
+      }
     }
   }
 }
