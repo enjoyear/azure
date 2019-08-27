6124917c66eebeb5e4827cb4795b0691112849ce
Chen Guo <almightygc@gmail.com>
2019-08-27 16:32:45 -0700

    a

---
+++ chen-spark-jobs/src/main/scala/com/chen/guo/Fun.scala
@@ -0,0 +1,29 @@
+package com.chen.guo
+
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{Executors, ThreadFactory}
+
+object Fun extends App {
+  private val threadId: AtomicInteger = new AtomicInteger(0)
+  private val threadPool = Executors.newFixedThreadPool(3, new ThreadFactory {
+    override def newThread(r: Runnable): Thread = {
+      new Thread(r, s"Thread-${threadId.incrementAndGet()}")
+    }
+  })
+
+  threadPool.submit(new SleepTask(2000))
+  threadPool.submit(new SleepTask(5000))
+  threadPool.submit(new SleepTask(2000))
+  threadPool.submit(new SleepTask(3000))
+
+  threadPool.shutdown()
+
+  class SleepTask(sleep: Long) extends Runnable {
+    override def run(): Unit = {
+      println(s"Thread ${Thread.currentThread().getName} sleeping for $sleep milliseconds...")
+      Thread.sleep(sleep)
+      println(s"Thread ${Thread.currentThread().getName} wakes up after $sleep milliseconds")
+    }
+  }
+
+}
